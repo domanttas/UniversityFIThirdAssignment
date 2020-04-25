807ef24aa9d5ea0a0b4d98df5f2f71de04590180
Domantas.Giedraitis <domantas.giedraitis@vegvisir.lt>
2020-04-25 13:11:01 +0300

    Profit comparison

--- strategy.py
+++ strategy.py
@@ -1,30 +1,100 @@
-from utils import load, plot_data, plot_calculated_data
+from utils import load, plot_data, plot_calculated_data, plot_profit, get_ma, get_lower_band, get_upper_band, get_positions, plot_comparison
+import pandas as pd
 
 
-data = load('./data.csv')
+data = load('./second_data_sample.csv')
+starting_data = data.copy()
 
 
 # MA for 100 days
-data['100MA'] = data['Close'].rolling(window=100).mean()
+data['100MA'] = get_ma(data)
 # Bollinger's upper band
-data['UpperBand'] = data['100MA'] + \
-    (data['Close'].rolling(window=100).std() * 2)
+data['UpperBand'] = get_upper_band(data, 100, 2)
 # Bollinger's lower band
-data['LowerBand'] = data['100MA'] - \
-    (data['Close'].rolling(window=100).std() * 2)
+data['LowerBand'] = get_lower_band(data, 100, 2)
 
 
-data['Position'] = None
+data = get_positions(data)
 
 
-for row in range(len(data)):
-    if (data['Close'].iloc[row] > data['UpperBand'].iloc[row]) and (data['Close'].iloc[row-1] < data['UpperBand'].iloc[row-1]):
-        data['Position'] = -1
-    if (data['Close'].iloc[row] < data['LowerBand'].iloc[row]) and (data['Close'].iloc[row-1] > data['LowerBand'].iloc[row-1]):
-        data['Position'] = 1
-
-data['Position'].fillna(method='ffill', inplace=True)
+# Plot start data
+plot_data(data, 'Close', '100MA', 'UpperBand', 'LowerBand')
 
 
-# plot_data(data, 'Close', '100MA', 'UpperBand', 'LowerBand')
-plot_calculated_data(data, 'Close', '100MA', 'UpperBand', 'LowerBand')
+# Plot strategy data
+plot_calculated_data(data, 'Close', '100MA', 'UpperBand',
+                     'LowerBand', 'Without optimization')
+
+# Plot profit without optimisation
+data['Return'] = data['Close'].pct_change()
+data['Strategy return'] = data['Return'] * data['Position']
+plot_profit(data, 'Without optimization')
+
+
+# Brute force optimization
+ma_values = [10, 40, 70, 100]
+std_values = [0.5, 1, 1.5, 2, 2.5, 3]
+optimization_data = starting_data.copy()
+
+result_df = pd.DataFrame({
+    'window': [],
+    'std': [],
+    'result': []
+})
+
+for std_value in std_values:
+    for ma_value in ma_values:
+        optimization_data = starting_data.copy()
+        ma = get_ma(optimization_data)
+        upper_band = get_upper_band(optimization_data, ma_value, std_value)
+        lower_band = get_lower_band(optimization_data, ma_value, std_value)
+
+        optimization_data['100MA'] = ma
+        optimization_data['UpperBand'] = upper_band
+        optimization_data['LowerBand'] = lower_band
+
+        optimization_data = get_positions(optimization_data)
+
+        optimization_data['Return'] = optimization_data['Close'].pct_change()
+        optimization_data['Strategy return'] = optimization_data['Return'] * \
+            optimization_data['Position']
+
+        result_df = result_df.append({
+            'window': ma_value,
+            'std': std_value,
+            'result': optimization_data['Strategy return'].sum()
+        }, ignore_index=True)
+
+
+print(result_df.sort_values(by=['result'], ascending=False))
+
+# Plotting for best data from optimization
+best_row = result_df.loc[result_df['result'].idxmax()]
+best_ma = int(best_row['window'])
+best_std = best_row['std']
+
+print(best_std)
+print(best_ma)
+
+best_data = starting_data.copy()
+
+ma = get_ma(best_data)
+upper_band = get_upper_band(best_data, best_ma, best_std)
+lower_band = get_lower_band(best_data, best_ma, best_std)
+
+best_data['100MA'] = ma
+best_data['UpperBand'] = upper_band
+best_data['LowerBand'] = lower_band
+
+best_data = get_positions(best_data)
+
+# Plot strategy data
+plot_calculated_data(best_data, 'Close', '100MA',
+                     'UpperBand', 'LowerBand', 'Optimized')
+
+# Plot profit without optimisation
+best_data['Return'] = best_data['Close'].pct_change()
+best_data['Strategy return'] = best_data['Return'] * best_data['Position']
+
+plot_profit(best_data, 'Optimized')
+plot_comparison(data, best_data)
--- utils.py
+++ utils.py
@@ -23,15 +23,65 @@ def plot_data(data, value_name, indicator_name, upper, lower):
     plt.show()
 
 
-def plot_calculated_data(data, value_name, indicator_name, upper, lower):
+def plot_calculated_data(data, value_name, indicator_name, upper, lower, title):
+    selling_positions = data[data['Position'].isin([-1])]
+    buying_positions = data[data['Position'].isin([1])]
+
+    figure = plt.plot(
+        selling_positions['Date'], selling_positions[value_name], marker='x', color='blue')
+    figure = plt.plot(
+        buying_positions['Date'], buying_positions[value_name], marker='x', color='red')
+
     figure = plt.plot(data['Date'], data[value_name])
     figure = plt.plot(data['Date'], data[indicator_name])
     figure = plt.plot(data['Date'], data[upper])
     figure = plt.plot(data['Date'], data[lower])
 
-    figure = plt.legend(['Close', indicator_name, upper, lower])
-    figure = plt.title('Close price')
+    figure = plt.legend(['Sell', 'Buy', 'Close', indicator_name, upper, lower])
+    figure = plt.title(title)
     figure = plt.xlabel('Date')
     figure = plt.ylabel('Price')
+
+    plt.show()
+
+
+def plot_profit(data, title):
+    figure = plt.plot(data['Date'], data['Strategy return'].cumsum())
+    figure = plt.title(title)
+    plt.show()
+
+
+def get_ma(data):
+    return data['Close'].rolling(window=100).mean()
+
+
+def get_upper_band(data, window_number, std_number):
+    return get_ma(data) + (data['Close'].rolling(window=window_number).std() * std_number)
+
+
+def get_lower_band(data, window_number, std_number):
+    return get_ma(data) - (data['Close'].rolling(window=window_number).std() * std_number)
+
+
+def get_positions(data):
+    data['Position'] = None
+    for row in range(len(data)):
+        if (data['Close'].iloc[row] > data['UpperBand'].iloc[row]) and (data['Close'].iloc[row-1] < data['UpperBand'].iloc[row-1]):
+            data['Position'].iloc[row] = -1
+        if (data['Close'].iloc[row] < data['LowerBand'].iloc[row]) and (data['Close'].iloc[row-1] > data['LowerBand'].iloc[row-1]):
+            data['Position'].iloc[row] = 1
+
+    data['Position'].fillna(method='ffill', inplace=True)
+    return data
+
+
+def plot_comparison(data, optimized_data):
+    plt.plot(data['Date'], data['Strategy return'].cumsum())
+    plt.title('Default')
+
+    plt.plot(optimized_data['Date'], optimized_data['Strategy return'].cumsum())
+    plt.title('Optimized')
+
+    plt.legend(['Default', 'Optimized'])
 
     plt.show()
